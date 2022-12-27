bc4d2720b8f48dcd71f573495232f661f3660eec
Christian Stockinger <mail@christian-stockinger.de>
2022-12-27 13:32:10 +0100

    feat(Basic-Tools): added Singletons and an MainThreadDispatcher

---
+++ Runtime/Base/Utils/Singleton.cs
@@ -0,0 +1,26 @@
+﻿
+namespace AdaptiveLoadingScreen.Base
+{
+    public class Singleton<T> where T : class, new()
+    {
+        private static T instance = null;
+
+        protected Singleton()
+        {
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+                return instance;
+            }
+        }
+    }
+}
+
+
